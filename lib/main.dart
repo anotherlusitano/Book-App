2e4bfea4f93edf4cdc65d1fa48d8f77923d9ffd7
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2023-04-23 12:41:02 +0100

    Delete old appBar & Add custom appBar

--- lib/main.dart
+++ lib/main.dart
@@ -31,15 +31,6 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Spooky Book App'),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.search),
-          ),
-        ],
-      ),
       body: Container(
         decoration: backgroundGradient(),
         child: Stack(
@@ -47,6 +38,11 @@ class HomePage extends StatelessWidget {
             Align(
               alignment: Alignment.bottomCenter,
               child: CustomBottomNav(),
+            ),
+            Column(
+              children: [
+                CustomBanner(),
+              ],
             ),
           ],
         ),
@@ -86,6 +82,36 @@ class CustomBottomNav extends StatelessWidget {
           Icon(Icons.person_outline),
           Icon(Icons.info_outline),
         ],
+      ),
+    );
+  }
+}
+
+class CustomBanner extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 16),
+        height: 90.0,
+        child: Column(
+          children: <Widget>[
+            const SizedBox(
+              height: 40,
+            ),
+            Row(
+              children: <Widget>[
+                const Text(
+                  'Book App',
+                ),
+                Expanded(
+                  child: Container(),
+                ),
+                const Icon(Icons.search),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
